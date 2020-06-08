8106088280fb0dbccd5290ea8ed16f8e8cf08795
Seth Donohue <sethdonohue@Admins-MBP.guest.corp.microsoft.com>
2020-06-08 14:36:08 -0700

    add carousel component extending tabs

--- packages/web-components/fast-components/src/index.ts
+++ packages/web-components/fast-components/src/index.ts
@@ -7,6 +7,7 @@ export * from "./breadcrumb-item/index";
 export * from "./button/index";
 export * from "./card/index";
 export * from "./combobox/index";
+export * from "./carousel/index";
 export * from "./checkbox/index";
 export * from "./color/index";
 export * from "./data-grid/index";
